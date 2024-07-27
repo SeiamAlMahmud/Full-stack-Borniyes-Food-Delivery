adbce71f26ad5a46518554b64eb40085a2007a7e
SeiamAlMahmud <samahmud49@gmail.com>
2024-07-27 02:31:27 +0000

    food Models

--- server/index.js
+++ server/index.js
@@ -1,1 +1,29 @@
+import express from "express"
+import cors from "cors"
+import { connectDB } from "./config/db.js"
 
+
+
+
+// app config
+const app = express()
+const port = process.env.PORT || 3000
+
+
+// middleware
+app.use(express.json())
+app.use(cors())
+
+// DB Connection
+connectDB()
+
+app.get("/",(req,res)=> {
+    res.send("Hello Word")
+})
+
+app.listen(port, ()=> {
+    console.log(`port start on ${port}`)
+})
+
+
+// mongodb+srv://<username>:<password>@first-project.dkbbjak.mongodb.net/?retryWrites=true&w=majority&appName=First-Project
