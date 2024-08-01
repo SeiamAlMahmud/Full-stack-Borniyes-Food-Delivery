c2531aafc121ef41fab7c9f59c702f01a21dd01b
SeiamAlMahmud <samahmud49@gmail.com>
2024-08-01 15:48:07 +0000

    backend_order_setup

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
 import "dotenv/config"
 import cartRouter from "./routes/cardroute.js"
+import orderRouter from "./routes/orderRoute.js"
 
 
 
@@ -19,13 +20,14 @@ app.use(express.json())
 app.use(cors())
 
 // DB Connection
-// connectDB()
+connectDB()
 
 //api endpoint
 app.use("/api/food", foodRouter)
 app.use("/images", express.static("uploads"))
 app.use("/api/user",userRouter)
 app.use("/api/cart",cartRouter)
+app.use("/api/order", orderRouter)
 
 
 
