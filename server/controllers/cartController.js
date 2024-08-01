b3dde22f582fa95d1468e2f39bf0fa323296fe89
SeiamAlMahmud <samahmud49@gmail.com>
2024-08-01 05:50:57 +0000

    add cart logic

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -6,9 +6,18 @@ const addToCart = async (req, res) => {
     try {
         let userData = await userModel.findOne({_id: req.body.userId})
         let cartData = userData.cardData;
-        
+        if(!cartData[req.body.itemId]){
+            cartData[req.body.itemId] = 1
+        }
+        else{
+            cartData[req.body.itemId] += 1;
+        }
+
+        await userModel.findByIdAndUpdate(req.body.userId , {cartData});
+        res.json({success: true, message:"Added To Cart"})
     } catch (error) {
-        
+        console.log(error);
+        res.json({success: false, message: "Error"})
     }
 }
 const removeFromCart = async (req, res) => {
