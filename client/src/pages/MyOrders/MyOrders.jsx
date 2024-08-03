c9a29a6538932d5832971c1240fdbd51788cd127
SeiamAlMahmud <samahmud49@gmail.com>
2024-08-03 16:11:17 +0000

    myCard component

--- client/src/pages/MyOrders/MyOrders.jsx
+++ client/src/pages/MyOrders/MyOrders.jsx
@@ -1,7 +1,8 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { foodStoreContext } from '../../context/StoreContext'
 import axios from 'axios'
-
+import { assets } from "../../assets/assets"
+import "./MyOrders.css"
 const MyOrders = () => {
     const [data, setData] = useState([])
     const { token, url } = useContext(foodStoreContext)
@@ -12,15 +13,38 @@ const MyOrders = () => {
         console.log(response)
     }
 
-    useEffect(()=> {
+    useEffect(() => {
         if (token) {
             fetchOrders()
         }
-    },[token])
+    }, [token])
 
     return (
-        <div>
+        <div className="my_orders">
+            <h2>My Orders</h2>
+            <div className="container_order">
+                {
+                    data.map((orders, idx) => {
+                        return (
+                            <div key={idx} className="my_orders_order">
+                                <img src={assets.parcel_icon} alt="" />
+                                <p>{orders.items.map((item, idex) => {
+                                    if (idex === orders.items.length - 1) {
+                                        return item.name + " X " + item.quantity
+                                    } else {
+                                        return item.name + " X " + item.quantity + ", "
 
+                                    }
+                                })}</p>
+                                <p>${orders.amount}.00</p>
+                                <p>Items: {orders.items.length}</p>
+                                <p><span>&#x25cf;</span> <b>{orders.status}</b></p>
+                                <button>Track Order</button>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
